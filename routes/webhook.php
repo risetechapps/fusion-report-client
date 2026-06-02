e2a7a98c7c7c06c75fecafeb192dd15b1d62b64f
risetechapps <apps@risetech.com.br>
2026-06-02 16:04:50 -0300

    Implementado suporte a parametros

--- routes/webhook.php
+++ routes/webhook.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use RiseTechApps\FusionReport\Http\Controllers\WebhookController;
 
-Route::middleware('api')
-    ->post('/fusion/webhook', WebhookController::class)
+Route::middleware(config('fusion-report.webhook.middleware', ['api']))
+    ->post(config('fusion-report.webhook.path', '/fusion/webhook'), WebhookController::class)
     ->name('fusion-report.webhook');
